568fb3cc53bdb7f891d370d759cc751462858986
Franco Negrete <franconegrete12@gmail.com>
2022-02-23 20:28:49 -0300

    Desafio 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+function Header() {
+    return <header className="header">
+            <a href="#" className="titulo"><h1>Titulo</h1></a>
+            <nav>
+                <a className="token" href="#">Token1</a>
+                <a className="token" href="#">Token2</a>  
+            </nav>                    
+    </header>
+}
+
+export default Header
